ae25dfdad92d373abdd90f3499f65c4c12e205d6
agus <agus.vendor.pasti@pegadaian.co.id>
2024-01-30 12:10:36 +0700

    fixing order jquery

--- src/components/SelectOption/AutoComplete.vue
+++ src/components/SelectOption/AutoComplete.vue
@@ -16,8 +16,8 @@
 
 <script>
     import "select2/dist/css/select2.min.css";
-    import "select2";
     import $ from "jquery";
+    import "select2";
     import {
         computed
     } from 'vue'
