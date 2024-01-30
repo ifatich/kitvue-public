74526667956d9aff4da699592c2c56afd3b81faf
Arkan <arkan.vendor.pasti@pegadaian.co.id>
2024-01-30 09:08:51 +0700

    [TEXT-AREA][FIX] Assign model, refactor code

--- src/components/Input/InputTextArea.vue
+++ src/components/Input/InputTextArea.vue
@@ -1,75 +1,21 @@
-<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import { defineOptions, defineModel, defineProps } from 'vue'
+
+defineOptions({ name: 'InputTextArea', inheritAttrs: false })
+
+const props = defineProps(['error', 'label', 'suffixIcon', 'class'])
+
+const model = defineModel()
+</script>
+
 <template>
-  <div class="group-input">
-    <label :for="id" class="form-label">
-      {{ label }}
+  <div :class="['group-input', props.class]">
+    <label v-if="label" :for="$attrs.id" class="form-label">
+      {{ props.label }}
     </label>
     <div class="input-group custom-input-group-icon lg">
-      <textarea
-        :type="type"
-        :class="['form-control text-area', classes]"
-        :id="id"
-        @input="$emit('input', $event.target.value)"
-        :disabled="disabled"
-        :required="required"
-        :error="error"
-        :placeholder="placeholder"
-      />
+      <textarea class="'form-control text-area'" v-model="model" />
     </div>
+    <div v-if="props.error" class="error-text mt-1">{{ error }}</div>
   </div>
 </template>
-<script>
-import { computed, reactive } from 'vue'
-
-export default {
-  name: 'TextArea',
-  props: {
-    id: {
-      default: '150'
-    },
-    label: {
-      type: String
-    },
-    icon: {
-      type: String,
-      default: '/assets/images/icon-info.svg'
-    },
-    placeholder: {
-      type: String
-    },
-    value: {
-      type: String,
-      default: ''
-    },
-    required: {
-      type: Boolean,
-      default: false
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    error: {
-      type: Boolean,
-      default: false
-    },
-    errorMessage: {
-      type: String,
-      default: ''
-    },
-    type: {
-      type: String,
-      default: 'text'
-    }
-  },
-  setup(props) {
-    props = reactive(props)
-    return {
-      classes: computed(() => ({
-        [`${props.error ? 'is-invalid' : ''}`]: true
-      }))
-    }
-  }
-}
-</script>
-<style></style>
