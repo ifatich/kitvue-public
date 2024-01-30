6f4ff8b55b55084f83d7b165f824a2ecd99ff721
Arkan <arkan.vendor.pasti@pegadaian.co.id>
2024-01-30 08:03:43 +0700

    [INPUT-AREA][FIX] Remove require

--- src/components/Input/InputTextArea.vue
+++ src/components/Input/InputTextArea.vue
@@ -1,74 +1,77 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div class="group-input">
-        <label :for="id" class="form-label">
-            {{ title }}
-            <img :src="require('../../assets/images/icon-info.svg')" />
-        </label>
-        <div class="input-group custom-input-group-icon lg">
-            <!-- <div class="input-group-icon">
-                <img :src="require('../../assets/images/world.svg')" />
-            </div> -->
-            <textarea :type="type" :class="['form-control text-area', classes]" :id="id"
-                @input="$emit('input', $event.target.value)" :disabled="disabled" :required="required" :error="error"
-                :placeholder="['Masukkan ' + title.toLowerCase()]" />
-            </div>
+  <div class="group-input">
+    <label :for="id" class="form-label">
+      {{ title }}
+    </label>
+    <div class="input-group custom-input-group-icon lg">
+      <textarea
+        :type="type"
+        :class="['form-control text-area', classes]"
+        :id="id"
+        @input="$emit('input', $event.target.value)"
+        :disabled="disabled"
+        :required="required"
+        :error="error"
+        :placeholder="['Masukkan ' + title.toLowerCase()]"
+      />
     </div>
+  </div>
 </template>
 <script>
-import { computed, reactive } from "vue";
+import { computed, reactive } from 'vue'
 
 export default {
-    name: "TextArea",
-    props: {
-        id: {
-            default: "150",
-        },
-        title: {
-            type: String,
-            default: "Title",
-        },
-        icon: {
-            type: String,
-            default: "/assets/images/icon-info.svg",
-        },
-        placeholder: {
-            type: String,
-            default: "placeholder Textarea . . .",
-        },
-        value: {
-            type: String,
-            default: "",
-        },
-        required: {
-            type: Boolean,
-            default: false,
-        },
-        disabled: {
-            type: Boolean,
-            default: false,
-        },
-        error: {
-            type: Boolean,
-            default: false,
-        },
-        errorMessage: {
-            type: String,
-            default: "",
-        },
-        type: {
-            type: String,
-            default: "text",
-        },
+  name: 'TextArea',
+  props: {
+    id: {
+      default: '150'
     },
-    setup(props) {
-        props = reactive(props);
-        return {
-        classes: computed(() => ({
-            [`${props.error ? "is-invalid" : ""}`]: true,
-        })),
-        };
+    title: {
+      type: String,
+      default: 'Title'
     },
-};
+    icon: {
+      type: String,
+      default: '/assets/images/icon-info.svg'
+    },
+    placeholder: {
+      type: String,
+      default: 'placeholder Textarea . . .'
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: Boolean,
+      default: false
+    },
+    errorMessage: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    }
+  },
+  setup(props) {
+    props = reactive(props)
+    return {
+      classes: computed(() => ({
+        [`${props.error ? 'is-invalid' : ''}`]: true
+      }))
+    }
+  }
+}
 </script>
 <style></style>
