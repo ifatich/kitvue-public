fba6fd564d4a98bbdc0f51be80a47abeb591b5c5
Arkan <arkan.vendor.pasti@pegadaian.co.id>
2024-02-06 15:28:10 +0700

    [MODAL][FIX] Refactor modal comp

---
+++ src/components/Modal/ModalComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { defineOptions, defineProps, defineModel } from 'vue'
+import { BModal } from 'bootstrap-vue-next'
+
+defineOptions({ name: 'ModalComponent' })
+
+const props = defineProps(['title'])
+
+const model = defineModel()
+</script>
+
+<template>
+  <div>
+    <BModal v-model="model" :title="props.title">
+      <template #modal-header="{ close }">
+        <Button class="btn btn-outline-danger" @click="close()"
+          >Close Modal</Button
+        >
+      </template>
+
+      <template #default="{}">
+        <slot name="body"></slot>
+      </template>
+
+      <template v-slot:footer="{ hide }">
+        <slot name="footer" :hide="hide"></slot>
+      </template>
+    </BModal>
+  </div>
+</template>
