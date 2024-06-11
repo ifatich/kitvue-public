032e27376b5f40de68c2b0d0ce27f2863d41750a
agus <agus.vendor.pasti@pegadaian.co.id>
2024-06-11 16:22:05 +0700

    remove background step tabpembinaan

--- src/components/Navbar/TabPembinaan.vue
+++ src/components/Navbar/TabPembinaan.vue
@@ -151,7 +151,6 @@
         &.completed span.content {
             border: 1px solid var(--g-kit-lime-50);
             background-image: url(../../assets/icon/subtract.svg);
-            background-color: var(--g-kit-lime-50);
             background-size: cover;
             vertical-align: text-bottom;
             width: 19px;
