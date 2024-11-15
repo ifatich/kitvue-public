e86482fbcc9e21dbdfda8e9dd7ccb49364aea7fd
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2024-11-15 14:07:14 +0700

    fix(FilePickerLG): show data from file url images

--- src/components/Filepicker/FilePickerLG.vue
+++ src/components/Filepicker/FilePickerLG.vue
@@ -23,10 +23,16 @@
             <img class="drop-zoon__image" :src="previewImage" alt="Preview" />
         </div>
         <div v-else-if="fileName && !showPreview" class="custom-file-upload__file-name">
-            <img @click="remove" v-if="previewImage || fileName" class="close-img"
+            <div v-if="!showFileURL">
+                <img @click="remove" v-if="previewImage || fileName" class="close-img"
                 :class="{ 'd-block remove-button btn-close': previewImage || fileName }"
                 src="../../assets/icon/cross.svg" />
-            <span>{{ fileName }}</span>
+                <span>{{ fileName }}</span>
+            </div>
+            <div v-else class="d-flex align-items-center justify-content-between">
+                <span>{{ fileName }}</span>
+                <a @click="handleFileData" class="text-green show-preview">Lihat file</a>
+            </div>
         </div>
     </div>
     <div v-if="fileError" class="error-text">
@@ -61,7 +67,7 @@
                 fileError: null,
             };
         },
-        emits: ['fileDropped', 'fileRemoved', 'errorPermission'],
+        emits: ['fileDropped', 'fileRemoved', 'errorPermission',"showUrlData"],
         methods: {
             imagesPreview1(files) {
                 if (files && files[0]) {
@@ -112,14 +118,38 @@
                 this.$refs.file.value = null;
                 this.$emit("fileRemoved", this.uniqueKey);
             },
+            formatFileSize(sizeInBytes) {
+                if (sizeInBytes < 1024) {
+                    return `${sizeInBytes} Bytes`;
+                } else if (sizeInBytes < 1024 * 1024) {
+                    return `${(sizeInBytes / 1024).toFixed(2)} KB`;
+                } else {
+                    return `${(sizeInBytes / (1024 * 1024)).toFixed(2)} MB`;
+                }
+            },
         },
         watch: {
             file: {
-                handler(newFile) {
+                async handler(newFile) {
                     if (!newFile) {
-                        this.previewImage = null;
-                        this.fileName = null;
-                        this.fileError = null;
+                    this.previewImage = null;
+                    this.fileName = null;
+                    this.fileError = null;
+                    } else {
+                    const response = await fetch(newFile);
+                    if (response.ok) {
+                        this.urlFile = newFile;
+                        const urlParts = newFile.split("/");
+                        const filename = urlParts[urlParts.length - 1].split("?")[0];
+                        const blob = await response.blob();
+
+                        this.previewImage = URL.createObjectURL(blob);
+                        this.fileName = `${filename} - ${this.formatFileSize(blob.size)}`;
+                        this.showFileURL = true;
+                    } else {
+                        console.error("Failed to fetch file:", response.statusText);
+                        this.fileError = `Failed to load file: ${response.statusText}`;
+                    }
                     }
                 },
                 immediate: true,
@@ -174,6 +204,13 @@
         font-weight: bold;
         opacity: 1;
         z-index: 999;
+    }
+
+    .show-preview {
+        z-index: 999;
+        cursor: pointer;
+        font-weight: 800;
+        font-size: 12px;
     }
 
     .fileName {
