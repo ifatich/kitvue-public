eda6bccd5446481fe576fe4fcc9d568465921d2c
Lord Milano <lm@Lords-MacBook-Pro.local>
2025-04-28 14:06:43 +0700

    update defact at input search and nik

--- src/components/Input/InputText.vue
+++ src/components/Input/InputText.vue
@@ -223,7 +223,10 @@
 	}
 
 	.search-active {
-		box-shadow: 0 0 0 1px #00883e;
-		border-radius: 4px;
+		.input-group {
+			box-shadow: 0 0 0 1px #00883e;
+			border-radius: 4px;
+			outline: none;
+		}
 	}
 </style>
