2ad782239d82b0b8669efbb98d545a4ea99f71f1
Lord Milano <lm@Lords-MacBook-Pro.local>
2025-08-20 15:54:35 +0700

    test update change vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig(
-  {
-    transpileDependencies: true
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+
+  css: {
+    loaderOptions: {
+      scss: {
+        // ini yang bakal sembunyikan warning dari node_modules
+        sassOptions: {
+          quietDeps: true,
+        },
+      },
+    },
   },
-)
+})
