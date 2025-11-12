15b234a7cc97853a24b3a94dc113e94414ecf9e5
Miftahul Ihsan <poj@MacBook-Pro-POJ.local>
2025-11-12 16:31:11 +0700

    adjustment datatable for waskat

--- src/components/Table/DataTable.vue
+++ src/components/Table/DataTable.vue
@@ -19,7 +19,7 @@
         <table ref="dataTable" class="table rounded-corners" style="width: 100%">
             <thead>
                 <tr>
-                      <th v-for="column in columns" :key="column.data" @click="handleSort(column.data)">
+                      <th   v-for="column in columns" :key="column.data" @click="handleSort(column.data)">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none">
                             <path d="M16.095 20.685C16.53 21.105 17.22 21.105 17.655 20.685L21.78 16.71C21.8915 16.6088 21.9816 16.4863 22.0449 16.3497C22.1082 16.2131 22.1434 16.0652 22.1485 15.9147C22.1536 15.7643 22.1284 15.6143 22.0745 15.4737C22.0206 15.3332 21.939 15.2048 21.8345 15.0964C21.7301 14.9879 21.605 14.9016 21.4665 14.8424C21.3281 14.7831 21.1792 14.7523 21.0286 14.7517C20.8781 14.7511 20.7289 14.7808 20.59 14.8389C20.4511 14.897 20.3253 14.9824 20.22 15.09L18 17.229V4.125C18 3.82663 17.8815 3.54048 17.6705 3.3295C17.4595 3.11853 17.1734 3 16.875 3C16.5766 3 16.2905 3.11853 16.0795 3.3295C15.8685 3.54048 15.75 3.82663 15.75 4.125V17.229L13.53 15.09C13.4247 14.9824 13.2989 14.897 13.16 14.8389C13.0211 14.7808 12.872 14.7511 12.7214 14.7517C12.5708 14.7523 12.4219 14.7831 12.2835 14.8424C12.1451 14.9016 12.0199 14.9879 11.9155 15.0964C11.8111 15.2048 11.7295 15.3332 11.6755 15.4737C11.6216 15.6143 11.5964 15.7643 11.6015 15.9147C11.6066 16.0652 11.6418 16.2131 11.7051 16.3497C11.7684 16.4863 11.8585 16.6088 11.97 16.71L16.095 20.685ZM7.92001 3.33C7.70908 3.11932 7.42314 3.00099 7.12501 3.00099C6.82689 3.00099 6.54095 3.11932 6.33001 3.33L2.20501 7.455C2.00629 7.66826 1.89811 7.95033 1.90325 8.24178C1.90839 8.53324 2.02646 8.81131 2.23258 9.01743C2.4387 9.22355 2.71678 9.34162 3.00823 9.34676C3.29968 9.3519 3.58175 9.24372 3.79501 9.045L6.00001 6.84V19.875C6.00001 20.1734 6.11854 20.4595 6.32952 20.6705C6.5405 20.8815 6.82665 21 7.12501 21C7.42338 21 7.70953 20.8815 7.92051 20.6705C8.13149 20.4595 8.25001 20.1734 8.25001 19.875V6.84L10.455 9.045C10.6683 9.24372 10.9503 9.3519 11.2418 9.34676C11.5332 9.34162 11.8113 9.22355 12.0174 9.01743C12.2236 8.81131 12.3416 8.53324 12.3468 8.24178C12.3519 7.95033 12.2437 7.66826 12.045 7.455L7.92001 3.33Z" fill="currentColor"/>
                         </svg>
@@ -32,16 +32,26 @@
                 <tr v-for="(item, index) in data" :key="index">
                     <td v-for="column in columns" :key="column.data" class="align-middle">
                            <template v-if="isDataArrayhasArrayObject(item[column.data])">
-                            <div class="text-link-data-table photo" @click="openModalTable(index)">
-                                Lihat Detail
-                            </div>
+                                <span v-html="getLimitedArrayObjectDisplay(item[column.data].rincianBarangJaminan, index)"></span>
+                                <div v-if="item[column.data].rincianBarangJaminan.length > 5" class="d-flex gap-1 align-items-center text-link-data-table" @click="isDataObjectArrayExpanded[index] = !isDataObjectArrayExpanded[index]">
+                                    {{isDataObjectArrayExpanded[index] ? `Sembunyikan` : `Lihat Selengkapnya`}}
+                                    <svg :class="[`icon-chevron`, isDataObjectArrayExpanded[index] && `rotate-180`]" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M8.1225 9.70253L12.0025 13.5825L15.8825 9.70253C16.2725 9.31253 16.9025 9.31253 17.2925 9.70253C17.6825 10.0925 17.6825 10.7225 17.2925 11.1125L12.7025 15.7025C12.3125 16.0925 11.6825 16.0925 11.2925 15.7025L6.7025 11.1125C6.3125 10.7225 6.3125 10.0925 6.7025 9.70253C7.0925 9.32253 7.7325 9.31253 8.1225 9.70253Z" fill="black"/>
+                                        <mask id="mask0_0_5346" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="6" y="9" width="12" height="7">
+                                            <path d="M8.1225 9.70253L12.0025 13.5825L15.8825 9.70253C16.2725 9.31253 16.9025 9.31253 17.2925 9.70253C17.6825 10.0925 17.6825 10.7225 17.2925 11.1125L12.7025 15.7025C12.3125 16.0925 11.6825 16.0925 11.2925 15.7025L6.7025 11.1125C6.3125 10.7225 6.3125 10.0925 6.7025 9.70253C7.0925 9.32253 7.7325 9.31253 8.1225 9.70253Z" fill="white"/>
+                                        </mask>
+                                        <g mask="url(#mask0_0_5346)">
+                                            <rect width="24" height="24" fill="currentColor"/>
+                                        </g>
+                                    </svg>
+                                </div>
                             </template>
                          <template v-else-if="isDataArrayObject(item[column.data])">
                             <div class="d-flex flex-column gap-1">
-                                <span v-html="getLimitedArrayDisplay(item[column.data])"></span>
-                                <div v-if="item[column.data].length > 5" class="d-flex gap-1 align-items-center text-link-data-table" @click="isDataExpanded= !isDataExpanded">
-                                    {{isDataExpanded ? `Sembunyikan` : `Lihat Selengkapnya`}}
-                                     <svg :class="[`icon-chevron`, isDataExpanded && `rotate-180`]" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <span v-html="getLimitedArrayDisplay(item[column.data], index)"></span>
+                                <div v-if="item[column.data].length > 5" class="d-flex gap-1 align-items-center text-link-data-table" @click="isDataArrayExpanded[index] = !isDataArrayExpanded[index]">
+                                    {{isDataArrayExpanded[index] ? `Sembunyikan` : `Lihat Selengkapnya`}}
+                                     <svg :class="[`icon-chevron`, isDataArrayExpanded[index] && `rotate-180`]" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M8.1225 9.70253L12.0025 13.5825L15.8825 9.70253C16.2725 9.31253 16.9025 9.31253 17.2925 9.70253C17.6825 10.0925 17.6825 10.7225 17.2925 11.1125L12.7025 15.7025C12.3125 16.0925 11.6825 16.0925 11.2925 15.7025L6.7025 11.1125C6.3125 10.7225 6.3125 10.0925 6.7025 9.70253C7.0925 9.32253 7.7325 9.31253 8.1225 9.70253Z" fill="black"/>
                                         <mask id="mask0_0_5346" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="6" y="9" width="12" height="7">
                                             <path d="M8.1225 9.70253L12.0025 13.5825L15.8825 9.70253C16.2725 9.31253 16.9025 9.31253 17.2925 9.70253C17.6825 10.0925 17.6825 10.7225 17.2925 11.1125L12.7025 15.7025C12.3125 16.0925 11.6825 16.0925 11.2925 15.7025L6.7025 11.1125C6.3125 10.7225 6.3125 10.0925 6.7025 9.70253C7.0925 9.32253 7.7325 9.31253 8.1225 9.70253Z" fill="white"/>
@@ -93,20 +103,23 @@
                             <LabelVue :text="item[rightContent]" variant="green" />
                         </div>
                         <div v-for="column in columns" :key="column.data" class="card-item">
-                            <span class="card-label" v-if="column.data !== leftContent && column.data !== rightContent">
+                            <span class="card-label" v-if="column.data !== leftContent && column.data !== rightContent && !column.hideMobile">
                                 {{ column.title }}
                             </span>
-                            <span class="card-value" v-if="column.data !== leftContent && column.data !== rightContent">
+                            <span class="card-value" v-if="column.data !== leftContent && column.data !== rightContent && !column.hideMobile">
                                 <template v-if="isDataArrayhasArrayObject(item[column.data])">
                                     <div class="text-link-data-table photo" @click="openModalTable(index)">
                                         Lihat Detail
                                     </div>
                                 </template>
                                 <template v-else-if="isDataArrayObject(item[column.data])">
-                                    <span class="text-end d-block" v-html="item[column.data].map(obj => `${obj.title} : ${obj.value}`).join('<br>')"></span>
-                                </template>
+                                    <span v-if="!column.arrayObjectCollapse" class="text-end d-block" v-html="item[column.data].map(obj => `${obj.title} : ${obj.value}`).join('<br>')"></span>
+                                    <div v-else class="text-link-data-table photo" @click="openModalHasilPemeriksaan(index)">
+                                        Lihat Detail
+                                    </div>
+                                </template> 
                                 <template v-else-if="isDataImg(item[column.data])">
-                                    <div class="text-link-data-table photo" @click="openModalSlider(index)">
+                                    <div class="text-link-data-table photo" @click="openModalSlider(index, column.modalLayerIndex) ">
                                         Lihat Foto
                                     </div>
                                 </template>
@@ -141,6 +154,7 @@
 </template>
 
 <script>
+
     import $ from "jquery";
     import "datatables.net-dt";
     import 'datatables.net-responsive-dt';
@@ -148,6 +162,10 @@
 
     import LabelVue from "../Label/Label.vue";
     import TablePagination from './TablePagination.vue';
+
+    import "../../assets/js/dataTables.rowsGroup.js";
+
+     window.$ = window.jQuery = $;
 
     export default {
         name: "DataTable",
@@ -162,10 +180,13 @@
                 entriesPerPage: 10,
                 currentPage: 1,
                 search: "",
-                isDataExpanded: false,
+                isDataObjectArrayExpanded: {},
+                isDataArrayExpanded: {},
                 isDataTwoLayers: false,
                 dataIndex: 0,
-                imgIndex: 0
+                imgIndex: 0,
+                sortOrder: "",
+                sortKey: "",
             };
         },
         props: {
@@ -183,26 +204,31 @@
             rightContent: {
                 type: String
             },
-            modalSliderOpen: {
+            modalSecondLayerOpen: {
                 type: Boolean,
-                default: false
             },
-            modalTableOpen: {
+            firstModalFirstLayerOpen: {
                 type: Boolean,
-                default: false
+            },
+            secondModalFirstLayerOpen: {
+                type: Boolean,
+            },
+            thirdModalFirstLayerOpen: {
+                type: Boolean,
             },
             mobileHeader: {
                 type: Boolean,
                 default: false
             },
-            selectedDataIndex: {
+            selectedFirstLayerIndex: {
                 type: Number,
-                default: 0
             },
-            selectedImgIndex: {
+            selectedSecondLayerIndex: {
                 type: Number,
-                default: 0
-            }
+            },
+            rowsPanColIndex: {
+                type: Array,
+            },
         },
         mounted() {
             this.initializeDataTable();
@@ -220,19 +246,25 @@
                     this.dataTableInstance.page(newValue - 1).draw('page');
                 }
             },
-            modalSliderOpen(newVal) {
+            modalSecondLayerOpen(newVal) {
               if(window.innerWidth < 560){
                 if (newVal === true) {
-                    this.$emit('update:modalTableOpen', false);
+                    this.$emit('update:firstModalFirstLayerOpen', false);
                 } else {
-                    this.$emit('update:modalTableOpen', true);
+                    this.$emit('update:firstModalFirstLayerOpen', true);
                 }
               }
             },
         },
         methods: {
-            getLimitedArrayDisplay(array) {
-                if (array.length > 5 && !this.isDataExpanded) {
+            getLimitedArrayDisplay(array, index) {
+                if (array.length > 5 && !this.isDataArrayExpanded[index]) {
+                    return array.slice(0, 5).map(obj => `${obj.title} : ${obj.value}`).join('<br>');
+                }
+                return array.map(obj => `${obj.title} : ${obj.value}`).join('<br>');
+            },
+            getLimitedArrayObjectDisplay(array, index) {
+                if (array.length > 5 && !this.isDataObjectArrayExpanded[index]) {
                     return array.slice(0, 5).map(obj => `${obj.title} : ${obj.value}`).join('<br>');
                 }
                 return array.map(obj => `${obj.title} : ${obj.value}`).join('<br>');
@@ -264,14 +296,11 @@
                     data.every(inner => isImageArray(inner)) // kasus array 2 dimensi
                 );
             },
-            isDataArrayhasArrayObject(data) {
-                return Array.isArray(data) &&
-                    data.every(item =>
-                    item &&
-                    typeof item === 'object' &&
-                    Object.values(item).some(value => Array.isArray(value))
-                );
-            },
+            isDataArrayhasArrayObject(obj) {
+                return obj 
+                    && typeof obj === "object"
+                    && Object.values(obj).some(v => Array.isArray(v));
+                },
 
             initializeDataTable() {
                 this.$nextTick(() => {
@@ -279,8 +308,11 @@
                         responsive: true,
                         searching: true,
                         info: true,
+                        order: [[0, "desc"]],
                         ordering: true,
                         paging: true,
+                        rowsGroup: this.rowsPanColIndex,
+                        
                     };
 
                     this.dataTableInstance = $(this.$refs.dataTable).DataTable(dataTableOptions);
@@ -322,18 +354,34 @@
                     this.dataTableInstance.destroy();
                 }
             },
-            openModalSlider(index) {
-                this.$emit('update:selectedImgIndex', index);
-                this.$emit('update:modalSliderOpen', true)
+            openModalHasilPemeriksaan(index){
+                this.$emit('update:selectedFirstLayerIndex', index);
+                this.$emit('update:secondModalFirstLayerOpen', true)
+            },
+            openModalSlider(index, layerIndex) {
+                if (this.selectedSecondLayerIndex === undefined) {
+                    this.openModalTable(index)
+                } else if (layerIndex === 0) {
+                     this.$emit('update:selectedFirstLayerIndex', index);
+                     this.$emit('update:thirdModalFirstLayerOpen', true)
+                } else {
+                    this.$emit('update:selectedSecondLayerIndex', index);
+                    this.$emit('update:modalSecondLayerOpen', true)
+                }
             },
             openModalTable(index){
-                this.$emit('update:selectedDataIndex', index);
-                this.$emit('update:modalTableOpen', true)
+                this.$emit('update:selectedFirstLayerIndex', index);
+                this.$emit('update:firstModalFirstLayerOpen', true)
             },
         }, 
         computed: {
             filteredCards() {
-                return this.data.filter(item =>
+
+                const uniqueBarang = this.data.filter((item, index, self) =>
+                    index === self.findIndex(obj => obj.id === item.id)
+                );
+
+                return uniqueBarang.filter(item =>
                     Object.values(item).some(value =>
                         String(value).toLowerCase().includes(this.searchQuery.toLowerCase())
                     )
