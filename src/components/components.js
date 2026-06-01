d06a8b494cf9e5c6a595c6211dd4416cddf89df8
Lord <ifatich@gmail.com>
2026-06-01 13:02:35 +0700

    1

--- src/components/components.js
+++ src/components/components.js
@@ -1,11 +1,16 @@
 import Accordion from './Accordion/Accordion.vue';
+import AccordionItem from './Accordion/AccordionItem.vue';
 import AddAmount from './AddAmount/AddAmountCounter.vue';
+import AddAmountItem from './AddAmount/AddAmountItem.vue';
+import AddAmountDenom from './AddAmount/AddAmountDenom.vue';
 import Alert from './Alert/Alert.vue';
 import AlertVarian from './Alert/AlertVarian.vue';
 import Aspect from './Aspect/Aspect.vue';
 import Breadcrumb from './Breadcrumb/Breadcrumb.vue';
 import Button from './Button/Button.vue';
 import ButtonSize from './Button/ButtonSize.vue';
+import ButtonRightSlider from './Button/ButtonRightSlider.vue';
+import ButtonLeftSlider from './Button/ButtonLeftSlider.vue';
 import CardAccount from './Card/CardAccount.vue';
 import CardArticle from './Card/CardArticle.vue';
 import CardArticleHighlight from './Card/CardArticleHighlight.vue';
@@ -20,6 +25,8 @@ import CardPromoCode from './Card/CardPromoCode.vue';
 import CardVoucher from './Card/CardVoucher.vue';
 import DatePicker from './Dropdown/DatePicker.vue';
 import Dropdown from './Dropdown/InputDropdown.vue';
+import InputDropdownMultiple from './Dropdown/InputDropdownMultiple.vue';
+import InputDropdownHeader from './Dropdown/InputDropdownHeader.vue';
 import FilePickerLG from './Filepicker/FilePickerLG.vue';
 import FilePickerSM from './Filepicker/FilePickerSM.vue';
 import InputPersen from './Input/InputNominalEnd.vue';
@@ -31,6 +38,7 @@ import InputSmallDate from './Input/InputSmallDate.vue';
 import ListGroupUnordered from './ListGroup/ListGroupUnordered.vue';
 import ListGroupUnit from './ListGroup/ListGroupUnit.vue';
 import ListOrdered from './ListGroup/ListGroupOrdered.vue';
+import ListSorted from './ListGroup/ListSorted.vue';
 import TableData from './Table/TableData.vue';
 import DataTable from './Table/DataTable.vue';
 import TableSticky from './Table/TableSticky.vue';
@@ -39,16 +47,72 @@ import Badge from './Badge/Badge.vue';
 import DropdownGroupItem from './Dropdown/DropdownGroupItem.vue';
 import DropdownItem from './Dropdown/DropdownItem.vue';
 import InputSearch from './Input/InputSearch.vue';
+import InputText from './Input/InputText.vue';
+import InputNominalStart from './Input/InputNominalStart.vue';
+import InputWithActionButton from './Input/InputWithActionButton.vue';
+import CalendarDropdown from './Input/CalendarDropdown.vue';
+import InputCamera from './Input/InputCamera.vue';
+import DateRangePicker from './Input/DateRangePicker.vue';
+import InputMonth from './Input/InputMonth.vue';
+import TimePickerResponsive from './Input/TimePickerResponsive.vue';
+import TimePicker from './Input/TimePicker.vue';
+import InputNIK from './Input/InputNIK.vue';
+import InputTimePicker from './Input/InputTimePicker.vue';
+import InputSearchQR from './Input/InputSearchQR.vue';
+import DateRangePickerOption from './Input/DateRangePickerOption.vue';
+import NewInputCamera from './Input/NewInputCamera.vue';
+import InputPhone from './Input/InputPhone.vue';
+import ContentTimePicker from './Input/ContentTimePicker.vue';
+import InputDatePicker from './Input/InputDatePicker.vue';
+import SideNavCMS from './Navbar/SideNavCMS.vue';
+import ItemSideNav from './Navbar/ItemSideNav.vue';
+import NavbarCorporate from './Navbar/NavbarCorporate.vue';
+import TabPembinaan from './Navbar/TabPembinaan.vue';
+import BerandaHeader from './Navbar/BerandaHeader.vue';
+import NavBackNavigator from './Navbar/NavBackNavigator.vue';
+import Tooltip from './Tooltip/TooltipComponent.vue';
+import Radio from './Radio/Radio.vue';
+import StackedBarChart from './Chart/StackedBarChart.vue';
+import DoughnutChart from './Chart/DoughnutChart.vue';
+import LineChart from './Chart/LineChart.vue';
+import BarChart from './Chart/BarChart.vue';
+import SideStepper from './SideStepper/SideStepper.vue';
+import SideStepperTest from './SideStepper/SideStepperTest.vue';
+import Checkbox from './Checkbox/CustomCheckbox.vue';
+import Label from './Label/Label.vue';
+import LabelIcon from './Label/LabelIcon.vue';
+import StepperVarian from './Stepper/StepperVarian.vue';
+import Stepper from './Stepper/Stepper.vue';
+import TabProduct from './Tab/TabProduct.vue';
+import TabGroup from './Tab/TabGroup.vue';
+import TabKategori from './Tab/TabKategori.vue';
+import ImageView from './Image/ImageView.vue';
+import AutoComplete from './SelectOption/AutoComplete.vue';
+import TablePagination from './Table/TablePagination.vue';
+import CustomTable from './Table/CustomTable.vue';
+import BottomSheet from './BottomSheet/GOffCanvas.vue';
+import Switch from './Switch/Switch.vue';
+import FilterCMS from './Filter/FilterCMS.vue';
+import InputFilter from './Filter/InputFilter.vue';
+import ModalLoad from './Modal/Load.vue';
+import ModalSlider from './Modal/ModalSlider.vue';
+import CustomModal from './Modal/CustomModal.vue';
+import HeaderCMS from './Header/HeaderCMS.vue';
 
 export default {
     Accordion,
+    AccordionItem,
     AddAmount,
+    AddAmountItem,
+    AddAmountDenom,
     Alert,
     AlertVarian,
     Aspect,
     Breadcrumb,
     Button,
     ButtonSize,
+    ButtonRightSlider,
+    ButtonLeftSlider,
     CardAccount,
     CardArticle,
     CardArticleHighlight,
@@ -63,23 +127,77 @@ export default {
     CardVoucher,
     DatePicker,
     Dropdown,
+    InputDropdownMultiple,
+    InputDropdownHeader,
     FilePickerLG,
     FilePickerSM,
     InputPersen,
     Input,
     InputKtp,
-    InputSmallDate,
-    InputSearch,
     TextArea,
     ModalComponent,
+    InputSmallDate,
     ListGroupUnordered,
     ListGroupUnit,
     ListOrdered,
+    ListSorted,
     TableData,
     DataTable,
     TableSticky,
     DataApps,
     Badge,
     DropdownGroupItem,
-    DropdownItem
+    DropdownItem,
+    InputSearch,
+    InputText,
+    InputNominalStart,
+    InputWithActionButton,
+    CalendarDropdown,
+    InputCamera,
+    DateRangePicker,
+    InputMonth,
+    TimePickerResponsive,
+    TimePicker,
+    InputNIK,
+    InputTimePicker,
+    InputSearchQR,
+    DateRangePickerOption,
+    NewInputCamera,
+    InputPhone,
+    ContentTimePicker,
+    InputDatePicker,
+    SideNavCMS,
+    ItemSideNav,
+    NavbarCorporate,
+    TabPembinaan,
+    BerandaHeader,
+    NavBackNavigator,
+    Tooltip,
+    Radio,
+    StackedBarChart,
+    DoughnutChart,
+    LineChart,
+    BarChart,
+    SideStepper,
+    SideStepperTest,
+    Checkbox,
+    Label,
+    LabelIcon,
+    StepperVarian,
+    Stepper,
+    TabProduct,
+    TabGroup,
+    TabKategori,
+    ImageView,
+    AutoComplete,
+    TablePagination,
+    CustomTable,
+    BottomSheet,
+    Switch,
+    FilterCMS,
+    InputFilter,
+    ModalLoad,
+    ModalSlider,
+    CustomModal,
+    HeaderCMS
 }
